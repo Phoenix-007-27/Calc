0b9de9bd279c401efe920f132a2ebf1a327b3cf8
Tim2788 <Sologov88@mail.ru>
2023-05-22 15:19:54 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,21 @@ public class Main {
         String symbol;
 
         ArabInRom convertToRom = new ArabInRom();
-        RomanToArabicConverter convertRomToArab = new RomanToArabicConverter();
+//        RomanToArabicConverter convertRomToArab = new RomanToArabicConverter();
         IsNumeric isNumeric = new IsNumeric();
+        Valid valid = new Valid();
 
         Scanner in = new Scanner(System.in);
         String numStr = in.nextLine();
         in.close();
 
-
-        try {
-            arrStr = numStr.split(" ");
-        } catch (Exception e) {
-        }
-        if (arrStr.length !=3) {
-            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор");
-        }
+        arrStr = valid.lengh(numStr);
         symbol = arrStr[1];
         if (isNumeric.isNumeric(arrStr[0]) && isNumeric.isNumeric(arrStr[2])) {
-            try {
-                a = Integer.parseInt(arrStr[0]);
-                b = Integer.parseInt(arrStr[0]);
-            } catch (Exception e) {
-              }
-            if ((a < 1 || a > 10) || (b < 1 || b > 10)) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
+//
+            a = valid.A(arrStr[0]);
+            b = valid.B(arrStr[2]);
+
 
             switch (symbol) {
                 case "+":
@@ -54,15 +44,9 @@ public class Main {
             }
 
         } else {
-//
-            try {
-                a = convertRomToArab.convertToArabic(arrStr[0]);
-                b = convertRomToArab.convertToArabic(arrStr[2]);
-            } catch (Exception e) {
-            }
-            if ((a < 1 || a > 10) || (b < 1 || b > 10)) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
+
+              a = valid.toArabA(arrStr[0]);
+                b = valid.toArabB(arrStr[2]);
 
         switch (symbol) {
             case "+":
@@ -76,7 +60,7 @@ public class Main {
                 if ((a-b) == 0) {
                     throw new Exception("давай ты не будешь вычитать равные римские цифры в ноль");
                 }
-//                System.out.println(convertToRom.convert(a - b));
+
                 break;
             case "*":
                 System.out.println(convertToRom.convert(a * b));
---
+++ src/Valid.java
@@ -0,0 +1,64 @@
+public class Valid {
+
+    RomanToArabicConverter convertRomToArab = new RomanToArabicConverter();
+    public String[] lengh(String str) throws Exception {
+        String[] arg = new String[0];
+        try {
+            arg = str.split(" ");
+        } catch (Exception e) {
+        }
+        if (arg.length !=3) {
+            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор");
+        }
+        return arg;
+    }
+
+    public int A(String argum) throws Exception {
+
+        int a = 0;
+        try {
+            a = Integer.parseInt(argum);
+        } catch (Exception e) {
+        }
+        if ((a < 1 || a > 10)) {
+            throw new Exception("Числа должны быть от 1 до 10 включительно");
+        }
+        return a;
+    }
+    public int B(String argum) throws Exception {
+        int b = 0;
+        try {
+            b = Integer.parseInt(argum);
+        } catch (Exception e) {
+        }
+        if ((b < 1 || b > 10)) {
+            throw new Exception("Числа должны быть от 1 до 10 включительно");        }
+        return b;
+    }
+
+    public int toArabA(String argum) throws Exception {
+        int a = 0;
+        try {
+                a = convertRomToArab.convertToArabic(argum);
+
+            } catch (Exception e) {
+            }
+            if ((a < 1 || a > 10) ) {
+                throw new Exception("Числа должны быть от 1 до 10 включительно");
+            }
+            return a;
+    }
+
+    public int toArabB(String argum) throws Exception {
+        int b = 0;
+        try {
+            b = convertRomToArab.convertToArabic(argum);
+        } catch (Exception e) {
+        }
+        if ((b < 1 || b > 10)) {
+            throw new Exception("Числа должны быть от 1 до 10 включительно");
+        }
+        return b;
+    }
+
+}
